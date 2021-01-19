949007477939e4f43e6000fa14114411abda8681
Gustavo <gustavodacorte@gmail.com>
2021-01-19 20:44:33 +0900

    first practical example in PHP

---
+++ php/quadratic_solver/helper.php
@@ -0,0 +1,45 @@
+<?php
+
+
+function quadraticSolver($a, $b, $c){
+    // Initiating delta with its actual value
+    $delta = $b**2 - (4*$a*$c);
+
+    $re = -$b/(2*$a);
+    $im = sqrt(abs($delta))/(2*$a);
+
+    // Instantiating variables
+    $roots = [
+        0,
+        0
+    ];
+
+    // Dual Root
+    if ($delta==0){
+        $result  = -$b/(2*$a);
+        $roots = [
+            $re,
+            $re,
+        ];
+    }
+
+    // Real Root
+    if ($delta>0){
+        $roots = [
+            $re+$im,
+            $re-$im,
+        ];
+    }
+
+    // Imaginary Root
+    if ($delta<0){
+        $roots = [
+            $re . " + " . $im . " i",
+            $re . " - " . $im . " i",
+        ];
+    }
+
+    return $roots;
+}
+
+?>
---
+++ php/quadratic_solver/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'helper.php';
+
+
+$result1 = quadraticSolver(1, 5, 6);
+$result2 = quadraticSolver(1, -8, 16);
+$result3 = quadraticSolver(1, 4, 5);
+
+
+echo "<pre>";
+echo json_encode($result1);
+echo "<br>";
+echo json_encode($result2);
+echo "<br>";
+echo json_encode($result3);
+
+?>
